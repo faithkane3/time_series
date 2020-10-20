b05c719cbb7c19ec75a16ba1c83b7c52fb2c2346
Faith Kane <faith.x.kane@gmail.com>
2020-10-20 16:27:04 -0500

    Commit prepare with old functions cleaned out.

--- ts_prepare.py
+++ ts_prepare.py
@@ -26,7 +26,7 @@ def numeric_hists(df, bins=20):
     plt.suptitle('Numeric Column Distributions')
     plt.show()
 
-######################## Function to acquire df WITHOUT DateTimeIndex and prep ####################
+######################## Function to acquire df and prep store data ####################
 
 def preppeed_store_df():
     """
@@ -57,33 +57,3 @@ def preppeed_store_df():
     
     # Display distributions of numeric columns
     numeric_hists(df)
-    
-########################### Function to acquire df WITH DateTimeIndex and prep #####################
-
-def prepped_dti_store_df():
-    """
-    Function to acquire and prepare
-    store df with DateTimeIndex and show
-    distributions for numeric columns
-    """
-    # Acquire the df
-    df = df.get_store_data()
-    
-    # Create date part columns
-    df['month'] = df.index.month
-    df['weekday'] = df.index.day_name()
-    
-    # Create calculated columns
-    df = df.assign(sales_total = df.sale_amount * df.item_price)
-    df = df.assign(sales_diff = df.sales_total.diff(periods=1))
-    
-    # Change dtypes of numeric columns to object and category
-    df = (df.astype({'sale_id': object, 'store_id': object, 
-                     'store_zipcode': object, 'item_id': object, 
-                     'item_upc12': object, 'item_upc14': object, 
-                     'month': 'category', 'weekday': 'category'}))
-    
-    # Display distributions of numeric columns
-    numeric_hists(df)
-    
-    return df
