644e60154b74053c553b8964dc1298678cbf42b6
Faith Kane <faith.x.kane@gmail.com>
2020-10-20 13:58:35 -0500

    Commit acquire notebook and module complete and working on prepare.

--- ts_acquire.py
+++ ts_acquire.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import requests
 
+######################### Helper function used in create big_df ########################################
+
 def get_df(name):
     """
     This function takes in the string
@@ -31,16 +33,14 @@ def get_df(name):
     df.to_csv(name + '.csv')
     return df
 
-# Function that checks for a csv, and if one doesn't exist it creates one
-# The function should also create one large df using all three df
+########################### big_df function without DateTimeIndex ######################################
 
 def get_store_data():
     """
     This function checks for csv files
-    for items, sales, and stores, and 
-    if there are none, it creates them and 
-    merges them into one df that it writes
-    to csv and reads in the future
+    for items, sales, stores, and big_df 
+    if there are none, it creates them.
+    It returns one big_df of merged dfs.
     """
     # check for csv files or create them
     if os.path.isfile('items.csv'):
@@ -66,26 +66,78 @@ def get_store_data():
         df = pd.merge(sales_df, stores_df, left_on='store', right_on='store_id').drop(columns={'store'})
         df = pd.merge(df, items_df, left_on='item', right_on='item_id').drop(columns={'item'})
 
+        # write merged DateTime df with all data to directory for future use
+        df.to_csv('big_df.csv')
+        return df
+
+########################### big_df function with DateTimeIndex #########################################
+
+def get_store_data_dti():
+    """
+    This function checks for csv files
+    for items, sales, stores, and big_df 
+    if there are none, it creates them.
+    It returns one big_df of merged dfs
+    with sale_date as a DateTimeIndex.
+    """
+    # check for csv files or create them
+    if os.path.isfile('items.csv'):
+        items_df = pd.read_csv('items.csv', index_col=0)
+    else:
+        items_df = get_df('items')
+        
+    if os.path.isfile('stores.csv'):
+        stores_df = pd.read_csv('stores.csv', index_col=0)
+    else:
+        stores_df = get_df('stores')
+        
+    if os.path.isfile('sales.csv'):
+        sales_df = pd.read_csv('sales.csv', index_col=0)
+    else:
+        sales_df = get_df('sales')
+        
+    if os.path.isfile('big_df_dti.csv'):
+        df = pd.read_csv('big_df_dti.csv', parse_dates=True, index_col='sale_date')
+        return df
+    else:
+        # merge all of the DataFrames into one
+        df = pd.merge(sales_df, stores_df, left_on='store', right_on='store_id').drop(columns={'store'})
+        df = pd.merge(df, items_df, left_on='item', right_on='item_id').drop(columns={'item'})
+
         # convert sale_date to DateTime Index
         df['sale_date'] = pd.to_datetime(df.sale_date)
         df = df.set_index('sale_date').sort_index()
 
         # write merged DateTime df with all data to directory for future use
-        df.to_csv('big_df.csv')
+        df.to_csv('big_df_dti.csv')
         return df
+    
+############################## opsd energy function without DataTimeIndex #############################
 
-# Function that checks for a csv, and if it doesn't exist it reads url and creates one
-# Function returns the df with a DateTime Index
+def opsd_germany_daily():
+    """
+    This function uses or creates the 
+    opsd_germany_daily csv and returns a df.
+    """
+    if os.path.isfile('opsd_germany_daily.csv'):
+        df = pd.read_csv('opsd_germany_daily.csv', index_col=0)
+    else:
+        url = 'https://raw.githubusercontent.com/jenfly/opsd/master/opsd_germany_daily.csv'
+        df = pd.read_csv(url)
+        df.to_csv('opsd_germany_daily.csv')
+    return df
 
-def german_energy_csv():
+############################## opsd energy function with DataTimeIndex ################################
+
+def opsd_germany_daily_dti():
     """
-    This function returns a df with a datetime index
-    using the opsd_germany url/csv.
+    This function uses or creates the 
+    opsd_germany_daily csv and returns a df with DataTimeIndex.
     """
-    if os.path.isfile('german_energy.csv'):
-        df = pd.read_csv('german_energy.csv', parse_dates=True, index_col=0)
+    if os.path.isfile('opsd_germany_daily_dti.csv'):
+        df = pd.read_csv('opsd_germany_daily_dti.csv', parse_dates=True, index_col=0)
     else:
         url = 'https://raw.githubusercontent.com/jenfly/opsd/master/opsd_germany_daily.csv'
         df = pd.read_csv(url, parse_dates=True).set_index('Date').sort_index()
-        df.to_csv('german_energy.csv')
+        df.to_csv('opsd_germany_daily_dti.csv')
     return df
--- ts_prepare.py
+++ ts_prepare.py
@@ -13,7 +13,7 @@ from datetime import timedelta, datetime as dt
 
 from ts_acquire import get_store_data
 
-# Function for numeric distributions
+############################# Function for numeric distributions #################################
 
 def numeric_hists(df, bins=20):
     """
@@ -26,12 +26,44 @@ def numeric_hists(df, bins=20):
     plt.suptitle('Numeric Column Distributions')
     plt.show()
 
-# Function to acquire and prep store df
+######################## Function to acquire df WITHOUT DateTimeIndex and prep ####################
 
-def prepped_store_df():
+def preppeed_store_df():
     """
     Function to acquire and prepare
     store dataframe and show
+    distributions for numeric columns
+    """
+    # Acquire the df
+    df = df.get_store_data()
+    
+    # Convert sale_date to DateTimeIndex
+    df['sale_date'] = pd.to_datetime(df.sale_date)
+    df = df.set_index('sale_date').sort_index()
+    
+    # Create date part columns
+    df['month'] = df.index.month
+    df['weekday'] = df.index.day_name()
+    
+    # Create calculated columns
+    df = df.assign(sales_total = df.sale_amount * df.item_price)
+    df = df.assign(sales_diff = df.sales_total.diff(periods=1))
+    
+    # Change dtypes of numeric columns to object and category
+    df = (df.astype({'sale_id': object, 'store_id': object, 
+                     'store_zipcode': object, 'item_id': object, 
+                     'item_upc12': object, 'item_upc14': object, 
+                     'month': 'category', 'weekday': 'category'}))
+    
+    # Display distributions of numeric columns
+    numeric_hists(df)
+    
+########################### Function to acquire df WITH DateTimeIndex and prep #####################
+
+def prepped_dti_store_df():
+    """
+    Function to acquire and prepare
+    store df with DateTimeIndex and show
     distributions for numeric columns
     """
     # Acquire the df
